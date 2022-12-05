68c657c9bf3854072537751ed6699b486fa93481
Oleg Chudilin <73231661+EraserH@users.noreply.github.com>
2022-12-05 23:46:41 +0400

    initial commit

---
+++ Server/Server/Crs/CoordinateSequenceFilter.cs
@@ -0,0 +1,25 @@
+﻿using NetTopologySuite.Geometries;
+using ProjNet.CoordinateSystems.Transformations;
+
+namespace ToSamaraApiServer.Crs
+{
+    class CoordinateSequenceFilter : ICoordinateSequenceFilter
+    {
+        private readonly MathTransform _mathTransform;
+
+        public CoordinateSequenceFilter(MathTransform mathTransform) => _mathTransform = mathTransform;
+
+        public bool Done => false;
+
+        public bool GeometryChanged => true;
+
+        public void Filter(CoordinateSequence seq, int i)
+        {
+            double x = seq.GetX(i);
+            double y = seq.GetY(i);
+            _mathTransform.Transform(ref x, ref y);
+            seq.SetX(i, x);
+            seq.SetY(i, y);
+        }
+    }
+}
